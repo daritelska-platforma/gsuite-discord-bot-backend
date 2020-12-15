def5fc0ac0319399b108c89a4aa71e638c1cbfab
Martin Kovachev <miracle@nimasystems.com>
2020-12-15 12:48:10 +0200

    Reorganized some files

--- api/public/index.php
+++ api/public/index.php
@@ -20,18 +20,3 @@ $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
-
-
-
-
-/*
- * From initial commint.
- *
- * error_reporting(E_ALL);
- * ini_set('display_errors', 1);
- *
- * require_once('vendor/autoload.php');
- * require_once('demo1/cal.php');
- *
- * //echo "Hello World (Daritelska Platforma)!";
- */
---
+++ demo1/index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * From initial commint.
+ */
+ 
+ error_reporting(E_ALL);
+ ini_set('display_errors', 1);
+ 
+ require_once('vendor/autoload.php');
+ require_once('demo1/cal.php');
+ 
+ echo "Hello World (Daritelska Platforma)!";
