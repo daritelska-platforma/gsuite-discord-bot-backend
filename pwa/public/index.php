e36034e4201888fb1d897480a8b9024bc1cb3d11
Ovanes Budakyan <ovanes.budakyan@ffwagency.com>
2020-12-15 08:54:02 +0200

    api-platform-install: Files adjustments.

--- pwa/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-require_once('vendor/autoload.php');
-require_once('demo1/cal.php');
-
-//echo "Hello World (Daritelska Platforma)!";
